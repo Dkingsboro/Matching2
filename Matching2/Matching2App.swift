107e4e7aa1c1b8a98b2eeceb9454f552fec96798
Dkingsboro <99dman@gmail.com>
2024-04-14 15:25:22 -0400

    Initial Commit

---
+++ Matching2/Matching2App.swift
@@ -0,0 +1,17 @@
+//
+//  Matching2App.swift
+//  Matching2
+//
+//  Created by Dylan Kingsboro on 4/14/24.
+//
+
+import SwiftUI
+
+@main
+struct Matching2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
