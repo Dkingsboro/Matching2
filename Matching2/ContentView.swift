660516a702d1d0f57d42cc7e2a5635e61af22080
Dkingsboro <99dman@gmail.com>
2024-04-14 15:34:22 -0400

    Caught up to where I am in the lecture

--- Matching2/ContentView.swift
+++ Matching2/ContentView.swift
@@ -9,13 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, Heather!")
+        HStack {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
         }
+        .foregroundColor(.orange)
         .padding()
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    
+    var body: some View 
+    {
+        ZStack(content: 
+        {
+            if isFaceUp 
+            {
+                RoundedRectangle(cornerRadius: 12).foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12).strokeBorder(lineWidth: 2)
+                Text("👻").font(.largeTitle)
+            } 
+            else 
+            {
+                RoundedRectangle(cornerRadius: 12)
+            }
+        })
     }
 }
 
